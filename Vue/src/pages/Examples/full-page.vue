3a7e2b36fd6183245a03ea124dafe4562ffe3c65
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2026-09-01 15:02:10 +0400

    CSS: Add FullPage Chat `min-width`

--- Vue/src/pages/Examples/full-page.vue
+++ Vue/src/pages/Examples/full-page.vue
@@ -109,6 +109,7 @@ onMounted(() => {
     height: 100%;
     background-color: var(--dx-component-color-bg);
     width: 50%;
+    min-width: 22.45em;
     max-width: 900px;
     border-radius: 12px;
     border: var(--dx-border-width) solid var(--dx-color-border);
